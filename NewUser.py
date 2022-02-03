40e91fbeb93bbd64c47978efa0d24a718a276c63
TheBlackHunteraz207737 <98956773+TheBlackHunteraz207737@users.noreply.github.com>
2022-02-03 18:37:09 +0600

    Add files via upload

---
+++ NewUser.py
@@ -0,0 +1,17 @@
+a=str(input("User Name:"))
+b=str(input("Password:"))
+c=str(input("Your Number:"))
+
+print("Your User Name Is"+a)
+print("Your Password Is Not Strong.You Also Creat Storng Password When You Will be Entered This System Next Time")
+print("Your Number Is"+c)
+print("You Succesfully Entered This System")
+print("Welcome To My New System")
+print("Don'T Laugh My System🤗.I Am New In This Cyber World😇😇.If Yours Will Inspired Me😘..I Hope I Can Show You A New Something🥱🤟.That Will Be Different Thing For Your Experience🥱")
+
+														
+														
+				
+print("																									😈😈🥴🥴											😁😁																																																																")
+print("Al-Bida..  AllaH Hafez🥰🥰😴😴")
+
